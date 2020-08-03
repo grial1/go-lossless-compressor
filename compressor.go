8ce30320b32c1d3ca0716e35419870613838b27e
gaston <grial1@vera.com.uy>
2020-08-03 20:27:45 -0300

    super slow dev: First version of GreyImage

--- compressor.go
+++ compressor.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/grial1/go-lossless-compressor/src"
+)
+
 func main() {
+
+	gc := src.NewGreyImageFromFile("test.pgm")
+
+	fmt.Printf("%d, %d\n", gc.GetWidth(), gc.GetHeight())
+
+	fmt.Printf("%d, %d\n", gc.GetPixel(0, 0), gc.GetPixel(40, 100))
+
+	gc.Save("saved_test.pgm")
 
 }

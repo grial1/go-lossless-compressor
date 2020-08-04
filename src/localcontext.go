f91ad6d10f3aef3bc870ea7c3f8520e9212f1302
gaston <grial1@vera.com.uy>
2020-08-04 18:50:01 -0300

    newContextMask added

--- src/localcontext.go
+++ src/localcontext.go
@@ -18,3 +18,59 @@ func (c *ContextTable) GetMask(row, col int) ContextMask {
 	return (*c)[PixelPos{row, col}]
 
 }
+
+/// Factory of context table
+func newContextMask(N int) *ContextMask {
+
+	findAll := false
+	distance := 0
+	totalFound := 0
+
+	cm := new(ContextMask)
+
+	for {
+
+		distance++
+		found := 0
+		secDistance := 0
+
+		for secDistance <= distance && totalFound+found < N {
+
+			if distance > secDistance && totalFound+found < N {
+
+				*cm = append(*cm, PixelPos{0 - secDistance, 0 - distance})
+				found++
+
+			}
+			if totalFound+found < N {
+
+				*cm = append(*cm, PixelPos{0 - distance, 0 - secDistance})
+				found++
+
+			}
+			if secDistance > 0 && totalFound+found < N {
+
+				*cm = append(*cm, PixelPos{0 - distance, 0 + secDistance})
+				found++
+
+			}
+			secDistance++
+
+		}
+
+		if totalFound == N && found == 0 {
+			findAll = true
+		}
+
+		if findAll {
+			break
+		}
+	}
+
+	return cm
+
+}
+
+func applyMask(p PixelPos, cm ContextMask, width, height int) *[]PixelPos {
+	// TODO
+}
